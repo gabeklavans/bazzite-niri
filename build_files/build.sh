f756cb663dcf17ae738b6ef5ab45084723cc5349
gabe <gabeklav@fastmail.com>
2025-08-11 23:35:55 -0400

    first pass at getting niri working

--- build_files/build.sh
+++ build_files/build.sh
@@ -9,16 +9,22 @@ set -ouex pipefail
 # List of rpmfusion packages can be found here:
 # https://mirrors.rpmfusion.org/mirrorlist?path=free/fedora/updates/39/x86_64/repoview/index.html&protocol=https&redirect=1
 
-# this installs a package from fedora repos
-dnf5 install -y tmux 
+# remove kde plasma
+dnf5 -y remove plasma-workspace plasma-* kde-*
 
-# Use a COPR Example:
-#
-# dnf5 -y copr enable ublue-os/staging
-# dnf5 -y install package
-# Disable COPRs so they don't end up enabled on the final image:
-# dnf5 -y copr disable ublue-os/staging
-
-#### Example for enabling a System Unit File
+# setup niri
+dnf5 -y install					\
+	niri						\
+	xdg-desktop-portal-gtk		\
+	xdg-desktop-portal-gnome	\
+	gnome-keyring				\
+	nautilus					\
+	mako						\
+	waybar						\
+	swayidle					\
+	polkit-kde					\
 
 systemctl enable podman.socket
+systemctl --global add-wants niri.service waybar.service
+systemctl --global add-wants niri.service mako.service
+systemctl --global add-wants niri.service swayidle.service
